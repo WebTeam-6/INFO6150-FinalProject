c0fbaa79ad8c193ddb4e603fae904a98b2e1bf6f
Anusha-Kadali02 <kadali.a@northeastern.edu>
2023-12-04 10:21:34 -0500

    feat: stripe backend

--- server.js
+++ server.js
@@ -6,8 +6,9 @@ var mongoose = require("mongoose");
 var cors = require('cors');
 var  productRouter  = require('./routes/productRouter.js') ;
 var userRouter = require('./routes/userRoute')
-
 var bodyParser = require("body-parser");
+const Stripe = require("stripe");
+const stripe = Stripe('sk_test_51OJQvKAPl4YpXYxVfgVmncSENwXYfKKTRMxuJDGp64hxD8f4tqwjphj9z55fVxywGsWWU1XGGQnlIADsur22kO0W00Yg3nytwu')
 
 // const uri = "mongodb+srv://" + process.env.DB_USERNAME  + ":" + process.env.DB_PASSWORD + "@cluster0.ql2dquw.mongodb.net/Shilpkala?retryWrites=true&w=majority";
 const uri = "mongodb+srv://" + process.env.DB_USERNAME  + ":" + process.env.DB_PASSWORD + "@cluster0.ukrlfk9.mongodb.net/Shlipkala?retryWrites=true&w=majority";
@@ -33,3 +34,36 @@ connect();
 app.listen(8000,()=>{
     console.log("Server running");
 })
+app.post("/api/create-checkout-session", async (req, res) => {
+    const { product } = req.body;
+  
+    try {
+     const session = await stripe.checkout.sessions.create({
+        payment_method_types: ["card"],
+        line_items: [
+          {
+            price_data: {
+              currency: "inr",
+              product_data: {
+                name: product.name,
+              },
+              unit_amount: product.price * 100,
+            },
+            quantity: product.quantity,
+          },
+        ],
+        mode: "payment",
+        success_url: "http://localhost:3000/success",
+        cancel_url: "http://localhost:3000/cancel",
+        invoice_creation: {
+            enabled: true,
+          },
+      });
+      console.log(session);
+      res.json({ id: session.id });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: "Internal Server Error" });
+    }
+  });
+  
