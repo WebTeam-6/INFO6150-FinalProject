0f05073691d8cbd0f68cc62413d022a88f2d396c
Srujana Adapa <srujanaadapa@Srujanas-MacBook-Pro.local>
2023-12-05 15:57:37 -0500

    backend-admin dashboard statistics APIs created

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,7 @@ var userRouter = require('./routes/userRoute')
 var orderRoute = require('./routes/orderRoute.js')
 var bodyParser = require("body-parser");
 var cartRouter = require('./routes/cartRouter.js')
+var adminRouter = require('./routes/adminRouter.js')
 
 // const uri = "mongodb+srv://" + process.env.DB_USERNAME  + ":" + process.env.DB_PASSWORD + "@cluster0.ql2dquw.mongodb.net/Shilpkala?retryWrites=true&w=majority";
 const uri = "mongodb+srv://" + process.env.DB_USERNAME  + ":" + process.env.DB_PASSWORD + "@cluster0.ukrlfk9.mongodb.net/Shlipkala?retryWrites=true&w=majority";
@@ -20,6 +21,7 @@ app.use('/user',userRouter);
 app.use('/product', productRouter);
 app.use('/orders',orderRoute);
 app.use('/cart',cartRouter);
+app.use('/admin',adminRouter);
 
 async function connect(){
     try{
